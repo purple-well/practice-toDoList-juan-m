7a3559baa555b6c9c77d8339fe7ded1a3b200565
Chegg <your.email@example.com>
2026-07-28 23:29:42 -0500

    todo list react app juan m

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+import Header from "./components/Header";
+import ToDoList from "./components/ToDoList";
+
+const App = () => {
+    return (
+        <div className="app">
+            <Header />
+            <ToDoList />
+        </div>
+    );
+}
+export default App
