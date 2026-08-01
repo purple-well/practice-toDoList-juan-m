b221d25d864cde85dfcad52f6a6144a213176684
Chegg <your.email@example.com>
2026-07-31 19:20:12 -0500

    practice todo list juan m

---
+++ static-todo-react-app/components/ToDoList.jsx
@@ -0,0 +1,11 @@
+const ToDoList = () => {
+    return (
+        <ul>
+            <li> Learn React </li>
+            <li> Build a project </li>
+            <li> Read documentation </li>
+        </ul>
+    );
+}
+
+export default ToDoList;
